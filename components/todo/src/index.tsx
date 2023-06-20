de85bfd895b901445983d85723e090ec22944603
Phock <jakelequire@gmail.com>
2023-06-20 00:51:11 -0700

    ###

--- components/todo/src/index.tsx
+++ components/todo/src/index.tsx
@@ -1,10 +1,33 @@
+
+import ToDo from './todo'
 import style from '../styles/index.module.css'
 
 export default function Index(): JSX.Element {
 
     return (
         <div className={style.index}>
-        
+            <div className={style.primary_container}>
+                
+                <div className={style.todo_container}>
+                    <ToDo />
+                </div>
+
+                <div className={style.wrapper}>
+                    
+                    <div className={style.upcoming_container}>
+
+
+                    </div>
+
+                    <div className={style.completed_container}>
+
+
+                    </div>
+
+                </div>
+                
+
+            </div>
         </div>
     )
 }
---
+++ components/todo/src/todo.tsx
@@ -0,0 +1,36 @@
+
+import ToDoList from './todo_list';
+import style from '../styles/index.module.css'
+
+
+export default function ToDo(): JSX.Element {
+
+	return (
+		<div className={style._todo}>
+			<div className={style.list_container}>
+                <a className={style.new_list_button}>New List +</a>
+                <ol className={style.list}>
+                    <li className={style.list_item}>
+                        <a className={style.list_item_link}>List 1</a>
+                    </li>
+                    <li className={style.list_item}>
+                        <a className={style.list_item_link}>List 2</a>
+                    </li>
+                    <li className={style.list_item}>
+                        <a className={style.list_item_link}>List 3</a>
+                    </li>
+                </ol>
+            </div>
+
+			<div className={style.todo_wrapper}>
+				<div className={style.top_container}>
+
+                </div>
+
+				<div className={style.form_container}>
+                    <ToDoList />
+                </div>
+			</div>
+		</div>
+	);
+}
