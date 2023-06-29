6d2930f33e350f774ef122fa32d801560ce6708c
Phock <jakelequire@gmail.com>
2023-06-28 20:19:41 -0700

    ###

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Raleway } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
+const raleway = Raleway({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={raleway.className}>
           {children}
       </body>
     </html>
