8068dc7b1b13535286f466e2b2960aefc177876f
Jake L <114044604+jakelequire@users.noreply.github.com>
2023-07-06 12:07:36 -0700

    ###

--- components/todo/src/list.tsx
+++ components/todo/src/list.tsx
@@ -1,20 +1,33 @@
+"use client"
+import { useState, useEffect } from "react";
+import NewListTab from "./newListTab";
+
 import Image from "next/image";
 import style from "../styles/list.module.css";
 
 import _EDIT from "@/public/assets/edit.svg";
 import _CLOSE from "@/public/assets/close-w.svg";
 
+
+const handleNewList = () => {
+
+
+  
+}
+
+
 export default function List(): JSX.Element {
   return (
     <div className={style.list_container}>
       <a className={style.new_list_button}>New List +</a>
+
       <ol className={style.list}>
         <li className={style.list_item}>
           <div className={style.list_title}>
             <a className={style.list_item_edit}>
               <Image width={20} height={20} src={_EDIT} alt="edit" />
             </a>
-            <a className={style.list_item_link}>List 1</a>
+            <a className={style.list_item_link}>_PLACEHOLDER_VALUE_</a>
           </div>
           <a className={style.list_item_delete}>
             <Image width={20} height={20} src={_CLOSE} alt="delete" />
---
+++ components/todo/src/newListTab.tsx
@@ -0,0 +1,12 @@
+
+
+import style from './newListTab.module.css';
+
+export default function NewListTab(): JSX.Element {
+
+    return (
+        <div className={style.new_list_tab}>
+        
+        </div>
+    )
+}
