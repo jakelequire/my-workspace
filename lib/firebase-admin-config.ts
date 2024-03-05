da41dadf85674ec046a208a650a2646b6a4ee47d
Phock <114044604+jakelequire@users.noreply.github.com>
2024-03-04 21:05:43 -0800

    maybe ?¿

--- lib/firebase-admin-config.ts
+++ lib/firebase-admin-config.ts
@@ -1,7 +1,12 @@
 import { initializeApp, getApps, cert } from 'firebase-admin/app';
+import type { ServiceAccount } from 'firebase-admin';
 import * as admin from 'firebase-admin';
-const serviceAccount = require('../env/my-workspace-8b9e9-firebase-adminsdk-fmhtp-58f1131ec9.json');
 
+const serviceAccount: ServiceAccount = {
+    projectId: process.env.NEXT_PUBLIC_SERVICE_ACCOUNT_PROJECT_ID,
+    clientEmail: process.env.NEXT_PUBLIC_SERVICE_ACCOUNT_CLIENT_EMAIL,
+    privateKey: process.env.NEXT_PUBLIC_SERVICE_ACCOUNT_PRIVATE_KEY?.replace(/\\n/g, '\n'), // Handle newlines for privateKey
+};
 
 const firebaseAdminConfig = {
     credential: admin.credential.cert(serviceAccount)
--- lib/firebase-config.ts
+++ lib/firebase-config.ts
@@ -6,15 +6,23 @@ import { GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
+
 const firebaseConfig = {
-    apiKey: 'AIzaSyAz22UO2NfIC3ELy1n0zrCeAfZroA33p0o',
-    authDomain: 'my-workspace-8b9e9.firebaseapp.com',
-    projectId: 'my-workspace-8b9e9',
-    storageBucket: 'my-workspace-8b9e9.appspot.com',
-    messagingSenderId: '284008368707',
-    appId: '1:284008368707:web:9289d809032cb7758b62f6',
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
+
+Object.entries(firebaseConfig).forEach(([key, value]) => {
+    console.log(`\n[firebase-config.ts]\nkey: ${key},\nvalue: ${value}`)
+    if (!value) {
+        throw new Error(`Firebase configuration error: ${key} is not set.`);
+    }
+});
 
 // Initialize Firebase
 const firebase_app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
