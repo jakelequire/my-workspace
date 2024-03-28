b661c2489f077bb11fc72ef9add39de6de25a68c
Jake L <114044604+jakelequire@users.noreply.github.com>
2024-03-28 16:25:30 -0700

    scaffolding for Vercel service

---
+++ app/api/services/vercel/route.ts
@@ -0,0 +1,10 @@
+
+
+
+export async function GET(request: Request) {
+
+
+
+    return new Response(JSON.stringify("Hello, world!"))
+}
+
---
+++ server/vercel/vercelService.ts
@@ -0,0 +1,44 @@
+
+
+
+
+
+export class VercelService {
+    API_TOKEN = process.env.NEXT_PUBLIC_VERCEL_API_TOKEN;
+    PROJECT_ID: string;
+
+    constructor(projectId: string) {
+        this.PROJECT_ID = projectId;
+    }
+
+
+    /**
+     * @see https://vercel.com/docs/rest-api/endpoints/deployments#list-deployments-response
+     */
+    public async listDeployments() {
+        const response = await fetch("https://api.vercel.com/v6/deployments", {
+            method: "get",
+            headers: {
+                Authorization: "Bearer " + this.API_TOKEN,
+            },
+        })
+        if(!response.ok) { throw new Error("Error in fetching data.") }
+    }
+
+
+    /**
+     * @see https://vercel.com/docs/rest-api/endpoints/deployments#get-deployment-events
+     */
+    public async getDeploymentEvents() {
+        const response = await fetch(`/v3/deployments/${PROJECT_ID}/events`, {
+            method: "GET",
+            headers: {
+                Authorization: "Bearer " + this.API_TOKEN,
+            }
+        })
+        if(!response.ok) { throw new Error("Error in fetching data.") }
+    }
+
+
+
+}
