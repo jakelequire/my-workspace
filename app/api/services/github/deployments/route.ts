af4675bebdb2e622488372ebd4882c59e6e2559b
Jake L <114044604+jakelequire@users.noreply.github.com>
2024-03-29 10:58:49 -0700

    another hotfix

--- app/api/services/github/deployments/route.ts
+++ app/api/services/github/deployments/route.ts
@@ -5,7 +5,5 @@ import { GitHubService } from "@/server/github/githubService";
 export async function GET(request: Request) {
 
 
-    return new Response(JSON.stringify("Route Not In Use", {
-        status: 404
-    }));
+    return new Response(JSON.stringify("Route Not In Use"));
 }
