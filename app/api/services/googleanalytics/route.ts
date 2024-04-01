9b71cb7811a8843f19810d7a143c4ffc56a73c1d
Phock <114044604+jakelequire@users.noreply.github.com>
2024-03-31 21:49:33 -0700

    Update route.ts

--- app/api/services/googleanalytics/route.ts
+++ app/api/services/googleanalytics/route.ts
@@ -1,8 +1,8 @@
 import GoogleAnalyticsService from "@/server/googleAnalytics/googleAnalyticsService"
 
 export async function GET(request: Request) {
-    const googleAnalyticsService = new GoogleAnalyticsService()
-    const data = await googleAnalyticsService.requestData()
+    // const googleAnalyticsService = new GoogleAnalyticsService()
+    // const data = await googleAnalyticsService.requestData()
 
 
     return new Response(JSON.stringify(data))
