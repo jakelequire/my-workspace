15493b87cfc2fb2ebe36fc5956f5fabf3c535894
Phock <114044604+jakelequire@users.noreply.github.com>
2024-04-01 19:45:04 -0700

    ###

---
+++ app/api/services/vercel/logdrain/route.ts
@@ -0,0 +1,13 @@
+import { log } from 'next-axiom'
+
+
+export async function GET(request: Request) {
+    const foo = log.logEvents.map((logEvent) => {
+        return {
+            message: logEvent.message,
+            level: logEvent.level,
+        };
+    });
+    console.log("[GET] /api/services/vercel/logdrain/route.ts", foo)
+    return new Response(JSON.stringify(foo));
+}
--- server/vercel/vercelService.ts
+++ server/vercel/vercelService.ts
@@ -1,10 +1,10 @@
-import { VercelApi } from "@/types/servertypes";
-import { ExternalApi } from "@/types/apiTypes";
-import { format } from "date-fns";
+import { VercelApi } from '@/types/servertypes';
+import { ExternalApi } from '@/types/apiTypes';
+import { format } from 'date-fns';
 
 const timestampToDate = (timestamp: number) => {
-    return format(new Date(timestamp), "MM/dd/yy hh:mm a");
-}
+    return format(new Date(timestamp), 'MM/dd/yy hh:mm a');
+};
 
 export class VercelService {
     API_TOKEN = process.env.NEXT_PUBLIC_VERCEL_API_TOKEN;
@@ -16,61 +16,67 @@ export class VercelService {
     constructor() {
         // this.PROJECT_ID = projectId;
         this.responseDeploymentObj = {
-            name: "",
-            url: "",
-            created: "",
-            state: "",
-            inspectorUrl: "",
+            name: '',
+            url: '',
+            created: '',
+            state: '',
+            inspectorUrl: '',
             meta: {
-                githubCommitMessage: "",
-                githubRepo: "",
-                githubRepoVisibility: "",
+                githubCommitMessage: '',
+                githubRepo: '',
+                githubRepoVisibility: '',
             },
-            target: "",
-            created_at: "",
-            building_at: "",
-            ready_at: "",
+            target: '',
+            created_at: '',
+            building_at: '',
+            ready_at: '',
         };
 
         this.errorDeploymentObj = {
-            name: "",
-            url: "",
-            created: "",
-            state: "",
-            inspectorUrl: "",
+            name: '',
+            url: '',
+            created: '',
+            state: '',
+            inspectorUrl: '',
             aliasError: {
-                code: "",
-                message: "",
+                code: '',
+                message: '',
             },
             aliasAssigned: 0,
             isRollbackCandidate: false,
             meta: {
-                githubCommitMessage: "",
-                githubRepo: "",
-                githubRepoVisibility: "",
+                githubCommitMessage: '',
+                githubRepo: '',
+                githubRepoVisibility: '',
             },
-            target: "",
-            created_at: "",
-            building_at: "",
-            ready_at: "",
+            target: '',
+            created_at: '',
+            building_at: '',
+            ready_at: '',
         };
     }
-
 
     /**
      * @see https://vercel.com/docs/rest-api/endpoints/deployments#list-deployments-response
      */
-    public async listDeployments(): Promise<VercelApi.DeploymentResponse[] | VercelApi.DeploymentError> {
-        const response = await fetch(`https://api.vercel.com/v6/deployments?projectId=${this.PROJECT_ID}`, {
-            method: "get",
-            headers: {
-                Authorization: "Bearer " + this.API_TOKEN,
-            },
-        })
+    public async listDeployments(): Promise<
+        VercelApi.DeploymentResponse[] | VercelApi.DeploymentError
+    > {
+        const response = await fetch(
+            `https://api.vercel.com/v6/deployments?projectId=${this.PROJECT_ID}`,
+            {
+                method: 'get',
+                headers: {
+                    Authorization: 'Bearer ' + this.API_TOKEN,
+                },
+            }
+        );
         const data: ExternalApi.Vercel.ListDeploymentResponse = await response.json();
-        if(!response.ok) { throw new Error("Error in fetching data.") }
+        if (!response.ok) {
+            throw new Error('Error in fetching data.');
+        }
 
-        if(data.deployments[0].aliasError) { 
+        if (data.deployments[0].aliasError) {
             const deployments = data.deployments[0];
             this.errorDeploymentObj = {
                 name: deployments.name,
@@ -79,8 +85,8 @@ export class VercelService {
                 state: deployments.state,
                 inspectorUrl: deployments.inspectorUrl,
                 aliasError: {
-                    code: deployments.aliasError.code || "",
-                    message: deployments.aliasError.message || "",
+                    code: deployments.aliasError.code || '',
+                    message: deployments.aliasError.message || '',
                 },
                 aliasAssigned: deployments.aliasAssigned,
                 isRollbackCandidate: deployments.isRollbackCandidate,
@@ -93,7 +99,7 @@ export class VercelService {
                 created_at: timestampToDate(deployments.createdAt),
                 building_at: timestampToDate(deployments.buildingAt),
                 ready_at: timestampToDate(deployments.ready),
-            }
+            };
 
             return this.errorDeploymentObj;
         }
@@ -114,27 +120,38 @@ export class VercelService {
                 created_at: timestampToDate(deployment.createdAt),
                 building_at: timestampToDate(deployment.buildingAt),
                 ready_at: timestampToDate(deployment.ready),
-            }
-        })
+            };
+        });
         this.responseDeploymentObj = deploymentData[0];
-        
+
         return deploymentData;
     }
-
 
     /**
      * @see https://vercel.com/docs/rest-api/endpoints/deployments#get-deployment-events
      */
     public async getDeploymentEvents() {
         const response = await fetch(`/v3/deployments/${this.PROJECT_ID}/events`, {
-            method: "GET",
+            method: 'GET',
             headers: {
-                Authorization: "Bearer " + this.API_TOKEN,
-            }
-        })
-        if(!response.ok) { throw new Error("Error in fetching data.") }
+                Authorization: 'Bearer ' + this.API_TOKEN,
+            },
+        });
+        if (!response.ok) {
+            throw new Error('Error in fetching data.');
+        }
     }
 
+    public async specificDeploymentEvent() {
 
-
+        const response = await fetch(
+            `https://api.vercel.com/v3/deployments/${this.PROJECT_ID}/events?builds=1&delimiter=1&direction=backward&follow=1&limit=100&name=bld_cotnkcr76&since=1540095775941&statusCode=5xx&teamId=SOME_STRING_VALUE&until=1540106318643`,
+            {
+                headers: {
+                    Authorization: 'Bearer <TOKEN>',
+                },
+                method: 'get',
+            }
+        );
+    }
 }
