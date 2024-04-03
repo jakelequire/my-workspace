56a80a26803bdb43225aa0f994504cd66bf8f35f
Jake L <114044604+jakelequire@users.noreply.github.com>
2024-04-03 13:21:21 -0700

    notes to self for later

--- components/pages/home/job_tracker/jobsDisplay/jobsDisplay.tsx
+++ components/pages/home/job_tracker/jobsDisplay/jobsDisplay.tsx
@@ -6,6 +6,16 @@ import { DataTable } from '../datatable/datatable';
 export default function JobsDisplay(): JSX.Element {
     const { jobItem } = useJobTrackerContext();
 
+
+    /*
+    NOTE: 
+        Might need to look into this. At home, this is displaying fine but on the work PC
+        it's not displaying anything. (on the live site)
+
+        Clearing cache / clearing local DB / and logging out isn't helping resolve it.
+        This file contains what datatable is being displayed. Not sure if the issue is 
+        here or the datatable component itself.
+    */
     const savedJobs = jobItem.filter((job) => job.status === 'Saved');
     const appliedJobs = jobItem.filter((job) => job.status === 'Applied');
 
