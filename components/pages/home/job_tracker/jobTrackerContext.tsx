f4c965e87e991b3b73b1fdb845d403f17fd25c89
Jake L <114044604+jakelequire@users.noreply.github.com>
2024-04-03 11:10:58 -0700

    ####

--- components/pages/home/job_tracker/jobTrackerContext.tsx
+++ components/pages/home/job_tracker/jobTrackerContext.tsx
@@ -24,7 +24,7 @@ function useJobTrackerProvider() {
     
     useEffect(() => {
         setJobItem(jobList); 
-    }, [jobList]);
+    }, []);
 
     /* ------------------------------ */
     /* ######## Add Job Item ######## */
