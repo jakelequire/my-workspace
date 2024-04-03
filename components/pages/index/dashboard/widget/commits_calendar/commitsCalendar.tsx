b0338a0f18a293b3203bedb86f41ab5c87195193
Jake L <114044604+jakelequire@users.noreply.github.com>
2024-04-03 11:01:58 -0700

    ###

--- components/pages/index/dashboard/widget/commits_calendar/commitsCalendar.tsx
+++ components/pages/index/dashboard/widget/commits_calendar/commitsCalendar.tsx
@@ -1,12 +1,12 @@
 'use client';
-import { useGlobalContext } from '@/components/GlobalContext';
+// import { useGlobalContext } from '@/components/GlobalContext';
 
 
 
 export default function CommitsCalendar(): JSX.Element {
-    const { commitData } = useGlobalContext();
-
-    console.log("[CommitsCalendar] commitData:", commitData)
+    // const { commitData } = useGlobalContext();
+// 
+    // console.log("[CommitsCalendar] commitData:", commitData)
 
     return (
         <>
