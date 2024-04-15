b17001b6a6b2bef01133d21806b43cdab4c27f18
Jake L <114044604+jakelequire@users.noreply.github.com>
2024-04-15 15:12:24 -0700

    updated quickaccess widget

--- components/pages/index/dashboard/widget/quick_access/quickAccess.tsx
+++ components/pages/index/dashboard/widget/quick_access/quickAccess.tsx
@@ -1,15 +1,41 @@
 'use client';
-
+import { 
+    ReaderIcon,
+    BackpackIcon
+} from '@radix-ui/react-icons';
 
 
 export default function QuickAccess(): JSX.Element {
+    const applications = [
+        {
+            name: "Todo",
+            image: ReaderIcon,
+        },
+        {
+            name: "Emails",
+            image: ReaderIcon,
+        },
+        {
+            name: "Jobs",
+            image: BackpackIcon,
+        }
+    ]
 
+    const Application = applications.map((item, index) => {
+        return (
+            <div key={index} className='border'>
+                {item.image}
+                <p>{item.name}</p>
+            </div>
+        )
+    })
 
 
     return (
         <div className='flex justify-center flex-col h-full w-full'>
-        
-        
+            <div className='grid h-full w-full grid-cols-3 grid-rows-2 gap-4'>
+                <Application />
+            </div>
         </div>
     )
 }
