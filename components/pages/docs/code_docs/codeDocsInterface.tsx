19427046cc984ce99bed033cf266c295e2f6019c
Jake L <114044604+jakelequire@users.noreply.github.com>
2024-05-10 14:11:47 -0700

    (workpc) ###

--- components/pages/docs/code_docs/codeDocsInterface.tsx
+++ components/pages/docs/code_docs/codeDocsInterface.tsx
@@ -9,13 +9,13 @@ export default function CodeDocsInterface(): JSX. Element {
 
 
     return (
-        <div className='flex h-full w-full border'>
+        <div className='flex flex-col h-full w-full border'>
 
-            <div className='flex h-[15%] w-full border'>
+            <div className='flex h-[10%] w-full border'>
                 <TopNavbar />
             </div>
 
-            <div className='flex h-[85%] w-full border'>
+            <div className='flex h-[90%] w-full border'>
                 <div className='flex w-[20%] h-full'>
                     <Sidebar />
                 </div>
